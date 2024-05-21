fca34f6dfd5cae0f9f47c78a1e4799cc57d4b213
Arpana-Karki <arpanakarki561@gmail.com>
2024-05-21 19:42:02 +0545

    file added

---
+++ Web Development/Javascript/objects/index.js
@@ -0,0 +1,34 @@
+/*
+    objects = a collection of related properties or methods 
+    can represent real world objects(person,places,products,...)
+    objects ={
+        key: value
+        function(){}
+    }
+*/
+
+// e.g. 1
+const person = {
+    firstName: "Aayush",
+    lastName : "Karki",
+    age : 14,
+    isStudent: true,
+    says: function(){
+        console.log(`Hello! My name is ${this.firstName} ${this.lastName} 😎 .`)
+    },
+
+}
+person.says();
+
+//e.g. 2
+
+const person2 = {
+    firstName: "Appu",
+    lastName : "Karki",
+    age : 14,
+    isStudent: true,
+    says2: ()=>console.log(`Hello! My name is Arpana Karki.`),
+
+}
+person2.says2();
+console.log(person2.firstName);
