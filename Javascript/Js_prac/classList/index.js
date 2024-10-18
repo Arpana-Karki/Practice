c0306b682e644de41ec66afde6b7ad7e8339e2f6
Arpana-Karki <arpanakarki561@gmail.com>
2024-10-18 08:43:37 +0545

    updated

---
+++ Javascript/Js_prac/classList/index.js
@@ -0,0 +1,56 @@
+// classList = Element property in Javascript used to interact with an element's list of classes(CSS classes)
+// Allows you to make reusable classes for many elements across your webpage.
+
+//add()
+//remove()
+// toggle (Remove if present ,Add if not )
+// replace(oldClass , newClass)
+// contains()
+
+
+const heading1 = document.getElementById("heading1");
+let buttons = document.querySelectorAll(".myButtons");
+
+heading1.addEventListener("mouseover",event=>{
+    event.target.classList.add("hedHoever");
+});
+
+
+/* using add and remove
+
+buttons.forEach(button=>{
+    button.addEventListener("mouseover",event=>{
+
+        button.classList.add("hoever");
+    });
+});
+
+buttons.forEach(button=>{
+    button.addEventListener("mouseout",event=>{
+        button.classList.remove("hoever");
+    });
+});
+*/
+buttons.forEach(button=>{
+    button.addEventListener("mouseover",event=>{
+        event.target.classList.toggle("hoever");
+    });
+});
+
+buttons.forEach(button=>{
+    button.addEventListener("mouseout",event=>{
+        event.target.classList.toggle("hoever");
+    });
+});
+
+buttons.forEach(button=>{
+    button.addEventListener("click",event=>{
+
+        if(event.target.classList.contains("disabled")){
+            event.target.textContent ="disabled";
+        }
+        else{
+            event.target.classList.replace("enabled","disabled");
+        }
+    });
+});
